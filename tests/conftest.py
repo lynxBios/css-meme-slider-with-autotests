2d72516a8d9d31f51b9ee5f48dad30571101a51b
Tacciana <123711868+lynxBios@users.noreply.github.com>
2026-02-20 23:12:09 +0300

    ci: add github actions workflow for ui tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope='function')
@@ -12,10 +13,16 @@ def browser():
 
     print("\nStarting browser for test...")
     service = Service(ChromeDriverManager() .install())
+    options = Options()
+
+    options.add_argument('--headless')  # Run without graphical window
+    options.add_argument('--disable-gpu')  # Disable GPU
+    options.add_argument('--no-sandbox')  # Required for running in Linux containers
+    options.add_argument('--disable-dev-shm-usage')  # Solves memory shortage issues in containers
 
     # 2. Initialization: start browser
     # Manager will automatically find, download and set the driver path
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
     driver.implicitly_wait(10)
 
     # 3. Hand over control to test
